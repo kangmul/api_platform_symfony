fe226f620ab2316ec9b6941813d792afea3c989d
LAPTOP-8EUNS5KJ\cbn <muluk.dharmayana@gmail.com>
2022-03-11 16:54:43 +0700

    add migration file

---
+++ migrations/Version20220310083415.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220310083415 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, departemen_id INT NOT NULL, nik VARCHAR(15) NOT NULL, name VARCHAR(100) NOT NULL, dateofbirth DATE NOT NULL, INDEX IDX_5D9F75A13948BF3F (departemen_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A13948BF3F FOREIGN KEY (departemen_id) REFERENCES departemen (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE employee');
+    }
+}
